48227ee97ae2a8e76905e45eeb61f2ecc5cb28a3
Medicharla Ravi Kiran <vk8896992@gmail.com>
2026-09-07 06:39:44 +0530

    fix: make student daily challenge dates IST

--- frontend/src/pages/DailyChallenge.jsx
+++ frontend/src/pages/DailyChallenge.jsx
@@ -159,7 +159,7 @@ export default function DailyChallenge({ onSelectProblem }) {
             >
               <Code2 className="w-4 h-4" />
               {solved ? 'Review Solution' : inProgress ? 'Continue Solving' : 'Solve Challenge'}
-              <ArrowRight className="w-3.5 h-3.5'" />
+              <ArrowRight className="w-3.5 h-3.5" />
             </button>
           </div>
         </div>
